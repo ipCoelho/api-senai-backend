5254dc56336d0815c7e1f70b485ce89177f67616
ipCoelho <ipcoelho.personal@gmail.com>
2021-11-05 10:59:30 -0300

    feat: update and delete.

--- crud.php
+++ crud.php
@@ -2,40 +2,10 @@
     #Datebase Authentication file import.
     include('connection.php');
 
-    #Functions with ID criterion.
-    function readID($cod_pessoa, $link) {
-        $query = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = '$cod_pessoa';";
-
-        if ($sqlObject = mysqli_query($link, $query)) {
-            $arrayPessoa = mysqli_fetch_all($sqlObject);
-
-            if (!count($arrayPessoa) <= 0) {
-                echo json_encode(array(
-                    "status" => "sucess",
-                    "data" => $arrayPessoa
-                ));
-            } else {
-                echo json_encode(array(
-                    "status" => "expection",
-                    "data" => "Array nulo no banco de dados"
-                ));
-            }
-            
-        } else {
-            echo json_encode(array(
-                    "status"=>"error",
-                    "data"=>"Erro ao inserir dados no banco"
-                ) 
-            );
-        }
-    }
-
-    ################################################################################################
-
-    #Function to read the Date.
-    function read($link) {
+    #Function to select a row in the DB.
+    function read($SQLAUTHENTICATION) {
         $query = "SELECT * FROM tbl_pessoa";
-        $sqlObject = mysqli_query($link, $query);
+        $sqlObject = mysqli_query($SQLAUTHENTICATION, $query);
         $arrayPessoa = null;
 
         if (!$sqlObject === false) {
@@ -51,16 +21,44 @@
             echo json_encode(
                 array(
                     "status" => "failed",
-                    "data" => mysqli_error($link)
+                    "data" => mysqli_error($SQLAUTHENTICATION)
                 )
             );
         }
     }
+    #Function 'read' with ID criterion.
+    function readID($cod_pessoa, $SQLAUTHENTICATION) {
+        $query = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = '$cod_pessoa';";
+        $sqlObject = mysqli_query($SQLAUTHENTICATION, $query);
 
-    function create($NOME, $SOBRENOME, $EMAIL, $CELULAR, $FOTOGRAFIA, $link) {
+        if (!$sqlObject === false) {
+            $arrayPessoa = mysqli_fetch_all($sqlObject);
+
+            if (!count($arrayPessoa) <= 0) {
+                echo json_encode(array(
+                    "status" => "sucess",
+                    "data" => $arrayPessoa
+                ));
+            } else {
+                echo json_encode(array(
+                    "status" => "expection",
+                    "data" => "Dados nulo no banco de dados"
+                ));
+            }
+        }
+        else {
+            echo json_encode(array(
+                    "status"=>"error",
+                    "data"=>"Erro ao inserir dados no banco"
+                ) 
+            );
+        }
+    }
+    #Function to create a row in the DB.
+    function create($NOME, $SOBRENOME, $EMAIL, $CELULAR, $FOTOGRAFIA, $SQLAUTHENTICATION) {
         $query = "INSERT INTO tbl_pessoa(nome, sobrenome, email, celular, fotografia) VALUES ('$NOME', '$SOBRENOME', '$EMAIL', '$CELULAR', '$FOTOGRAFIA');";
 
-        if (!mysqli_query($link, $query) === false) {
+        if (!mysqli_query($SQLAUTHENTICATION, $query) === false) {
             echo json_encode( 
                 array(
                     "status"=>"sucess",
@@ -72,6 +70,56 @@
                 array(
                     "status"=>"error",
                     "data"=>"Erro ao inserir dados no banco"
+                ) 
+            );
+        }
+    }
+    #Function to update a row in the DB.
+    function update($cod_pessoa, $NOME, $SOBRENOME, $EMAIL, $CELULAR, $FOTOGRAFIA, $SQLAUTHENTICATION) {
+        $query = "UPDATE tbl_pessoa SET 
+            nome = '$NOME',
+            sobrenome = '$SOBRENOME', 
+            email = '$EMAIL', 
+            celular = '$CELULAR', 
+            fotografia = '$FOTOGRAFIA'
+            
+            WHERE cod_pessoa = '$cod_pessoa';
+        ";
+        $sqlObject = mysqli_query($SQLAUTHENTICATION, $query);
+        
+        if (!$sqlObject === false) {
+            echo json_encode( 
+                array(
+                    "status"=>"sucess",
+                    "data"=>"Dados atualizados com sucesso"
+                ) 
+            );
+        } 
+        else {
+            echo json_encode(
+                array(
+                    "status"=>"error",
+                    "data"=>"Erro ao atualizar dados no banco"
+                ) 
+            );
+        }
+    }
+    #Function to delete a row in the DB.
+    function delete($cod_pessoa, $SQLAUTHENTICATION) {
+        $query = "DELETE FROM tbl_pessoa WHERE cod_pessoa = '$cod_pessoa';";
+        $sqlResult = mysqli_query($SQLAUTHENTICATION, $query);
+        
+        if (!$sqlResult === false) {
+            echo json_encode(array(
+                "status" => "sucess",
+                "data" => "Dados deletados com sucesso"
+            ));
+        }
+        else {
+            echo json_encode(
+                array(
+                    "status"=>"error",
+                    "data"=>"Erro ao atualizar dados no banco"
                 ) 
             );
         }
