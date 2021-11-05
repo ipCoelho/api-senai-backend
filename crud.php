870055a37ff07ca940036678f18d805806fe61b7
ipCoelho <ipcoelho.personal@gmail.com>
2021-11-05 09:10:39 -0300

    feat: readID done.

--- crud.php
+++ crud.php
@@ -2,22 +2,52 @@
     #Datebase Authentication file import.
     include('connection.php');
 
-    #Function to read the Date.
-    function read($link) {
-        $query = "SELECT * FROM tbl_pessoa";
+    #Functions with ID criterion.
+    function readID($cod_pessoa, $link) {
+        $query = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = '$cod_pessoa';";
 
         if ($sqlObject = mysqli_query($link, $query)) {
             $arrayPessoa = mysqli_fetch_all($sqlObject);
 
-            #json_encode() is a method that converts array to json.
+            if (!count($arrayPessoa) <= 0) {
+                echo json_encode(array(
+                    "status" => "sucess",
+                    "data" => $arrayPessoa
+                ));
+            } else {
+                echo json_encode(array(
+                    "status" => "expection",
+                    "data" => "Array nulo no banco de dados"
+                ));
+            }
+            
+        } else {
+            echo json_encode(array(
+                    "status"=>"error",
+                    "data"=>"Erro ao inserir dados no banco"
+                ) 
+            );
+        }
+    }
+
+    ################################################################################################
+
+    #Function to read the Date.
+    function read($link) {
+        $query = "SELECT * FROM tbl_pessoa";
+        $sqlObject = mysqli_query($link, $query);
+        $arrayPessoa = null;
+
+        if (!$sqlObject === false) {
+            $arrayPessoa = mysqli_fetch_all($sqlObject);
+
             echo json_encode( 
                 array(
                     "status" => "sucess",
                     "data" => $arrayPessoa
                 ) 
             );
-        }
-        else {
+        } else {
             echo json_encode(
                 array(
                     "status" => "failed",
@@ -30,15 +60,14 @@
     function create($NOME, $SOBRENOME, $EMAIL, $CELULAR, $FOTOGRAFIA, $link) {
         $query = "INSERT INTO tbl_pessoa(nome, sobrenome, email, celular, fotografia) VALUES ('$NOME', '$SOBRENOME', '$EMAIL', '$CELULAR', '$FOTOGRAFIA');";
 
-        if (mysqli_query($link, $query)) {
+        if (!mysqli_query($link, $query) === false) {
             echo json_encode( 
                 array(
                     "status"=>"sucess",
                     "data"=>"Dados inseridos com sucesso"
                 ) 
             );
-        } 
-        else {
+        } else {
             echo json_encode(
                 array(
                     "status"=>"error",
