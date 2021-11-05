dfce01d65db2f2263b602ffbca65cfac7c6359bf
ipCoelho <ipcoelho.personal@gmail.com>
2021-11-05 11:00:12 -0300

    feat: update and delete added.

--- index.php
+++ index.php
@@ -12,11 +12,25 @@
     #Takes the action's type from the request.
     $action = $_REQUEST['action'];
 
+    /* How do things work here? 
+
+        * All the requests are being sent by the software Postman.
+            -> Postman is a software that helps the backend developers, it simulates the frontend and it's requisitions.
+
+            -> All the variables $_REQUEST[' '] are being sent by it. The key with the name 'action' sent the route that
+            the code should do, which it's defined by a switch structure. */
+
     switch ($action) {
+
         case 'read':
             read($link);
         break;
-        
+
+         case 'readID':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+            readID($cod_pessoa, $link);
+        break;
+
         case 'create':
             $nome = $_REQUEST['nome'];
             $sobrenome = $_REQUEST['sobrenome'];
@@ -25,6 +39,22 @@
             $fotografia = $_REQUEST['fotografia'];
 
             create($nome, $sobrenome, $email, $celular, $fotografia, $link);
+        break;
+
+        case 'update':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+            $nome = $_REQUEST['nome'];
+            $sobrenome = $_REQUEST['sobrenome'];
+            $email = $_REQUEST['email'];
+            $celular = $_REQUEST['celular'];
+            $fotografia = $_REQUEST['fotografia'];
+
+            update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $link);
+        break;
+
+        case 'delete':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+            delete($cod_pessoa, $link);
         break;
     }
 ?>
